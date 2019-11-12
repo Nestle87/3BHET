48b1e51bed0847c49a3d5d1a4f651aeacfc16b81
Nestle87 <reinha170164@sr.htlweiz.at>
2019-11-12 14:55:19 +0100

    Create Blinken.ino

---
+++ Blinken/Blinken.ino
@@ -0,0 +1,67 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(2, OUTPUT);
+pinMode(3, OUTPUT);
+pinMode(4, OUTPUT);
+pinMode(5, OUTPUT);
+pinMode(6, OUTPUT);
+pinMode(7, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+digitalWrite(2, HIGH);
+
+delay(50);
+
+digitalWrite(3, HIGH);
+
+delay(50);
+
+digitalWrite(4, HIGH);
+
+delay(50);
+
+digitalWrite(5, HIGH);
+
+delay(50);
+
+digitalWrite(6, HIGH);
+
+delay(50);
+
+digitalWrite(7, HIGH);
+
+delay(50);
+
+
+//---------------------------------------------------
+
+digitalWrite(2, LOW);
+
+delay(200);
+
+digitalWrite(3, LOW);
+
+delay(200);
+
+digitalWrite(4, LOW);
+
+delay(200);
+
+digitalWrite(5, LOW);
+
+delay(200);
+
+digitalWrite(6, LOW);
+
+delay(200);
+
+digitalWrite(7, LOW);
+
+delay(200);
+
+
+}
